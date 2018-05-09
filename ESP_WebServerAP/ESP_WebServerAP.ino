14235ba30f6ae225be142613cee90bfd0d954117
Tobi <tobias.frahm@cybus.io>
2018-05-09 10:43:49 +0200

    Webserver
    hello world

--- ESP_WebServerAP/ESP_WebServerAP.ino
+++ ESP_WebServerAP/ESP_WebServerAP.ino
@@ -1,209 +1,72 @@
-/*--------------------------------------------------
-HTTP 1.1 Webserver as AccessPoint for ESP8266 
-for ESP8266 adapted Arduino IDE
-
-by Stefan Thesen 08/2015 - free for anyone
-
-Does HTTP 1.1 with defined connection closing.
-Handles empty requests in a defined manner.
-Handle requests for non-exisiting pages correctly.
-
-This demo allows to switch two functions:
-Function 1 creates serial output and toggels GPIO2
-Function 2 just creates serial output.
-
-Serial output can e.g. be used to steer an attached
-Arduino, Raspberry etc.
---------------------------------------------------*/
-
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
-const char* ssid = "FloodSensor-Accesspoint";
-const char* password = "";  // set to "" for open access point w/o passwortd
+const char* ssid = "Boot";
+const char* password = "x3PT#/558?";
 
-unsigned long ulReqcount;
+ESP8266WebServer server(80);
 
+const int led = 13;
 
-// Create an instance of the server on Port 80
-WiFiServer server(80);
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
 
-void setup() 
-{
-  // setup globals
-  ulReqcount=0; 
-  
-  // prepare GPIO2
-  pinMode(2, OUTPUT);
-  digitalWrite(2, 0);
-  
-  // start serial
-  Serial.begin(9600);
-  delay(1);
-  
-  // AP mode
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
   server.begin();
+  Serial.println("HTTP server started");
 }
 
-void loop() 
-{ 
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) 
-  {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  Serial.println("new client");
-  unsigned long ultimeout = millis()+250;
-  while(!client.available() && (millis()<ultimeout) )
-  {
-    delay(1);
-  }
-  if(millis()>ultimeout) 
-  { 
-    Serial.println("client connection time-out!");
-    return; 
-  }
-  
-  // Read the first line of the request
-  String sRequest = client.readStringUntil('\r');
-  //Serial.println(sRequest);
-  client.flush();
-  
-  // stop client, if request is empty
-  if(sRequest=="")
-  {
-    Serial.println("empty request! - stopping client");
-    client.stop();
-    return;
-  }
-  
-  // get path; end of path is either space or ?
-  // Syntax is e.g. GET /?pin=MOTOR1STOP HTTP/1.1
-  String sPath="",sParam="", sCmd="";
-  String sGetstart="GET ";
-  int iStart,iEndSpace,iEndQuest;
-  iStart = sRequest.indexOf(sGetstart);
-  if (iStart>=0)
-  {
-    iStart+=+sGetstart.length();
-    iEndSpace = sRequest.indexOf(" ",iStart);
-    iEndQuest = sRequest.indexOf("?",iStart);
-    
-    // are there parameters?
-    if(iEndSpace>0)
-    {
-      if(iEndQuest>0)
-      {
-        // there are parameters
-        sPath  = sRequest.substring(iStart,iEndQuest);
-        sParam = sRequest.substring(iEndQuest,iEndSpace);
-      }
-      else
-      {
-        // NO parameters
-        sPath  = sRequest.substring(iStart,iEndSpace);
-      }
-    }
-  }
-  
-  ///////////////////////////////////////////////////////////////////////////////
-  // output parameters to serial, you may connect e.g. an Arduino and react on it
-  ///////////////////////////////////////////////////////////////////////////////
-  if(sParam.length()>0)
-  {
-    int iEqu=sParam.indexOf("=");
-    if(iEqu>=0)
-    {
-      sCmd = sParam.substring(iEqu+1,sParam.length());
-      Serial.println(sCmd);
-    }
-  }
-  
-  
-  ///////////////////////////
-  // format the html response
-  ///////////////////////////
-  String sResponse,sHeader;
-  
-  ////////////////////////////
-  // 404 for non-matching path
-  ////////////////////////////
-  if(sPath!="/")
-  {
-    sResponse="<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p></body></html>";
-    
-    sHeader  = "HTTP/1.1 404 Not found\r\n";
-    sHeader += "Content-Length: ";
-    sHeader += sResponse.length();
-    sHeader += "\r\n";
-    sHeader += "Content-Type: text/html\r\n";
-    sHeader += "Connection: close\r\n";
-    sHeader += "\r\n";
-  }
-  ///////////////////////
-  // format the html page
-  ///////////////////////
-  else
-  {
-    ulReqcount++;
-    sResponse  = "<html><head><title>Flood Sensor Start Config</title></head><body>";
-    sResponse += "<font color=\"#000000\"><body bgcolor=\"#d0d0f0\">";
-    sResponse += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">";
-    sResponse += "<h1>Flood Sensor Start Config</h1>";
-    sResponse += "<form>"
-    sResponse += SSID : "<input type='text' name='SSID'><br>"
-    sResponse += PASSWORD : "<input type='password' name='PASSWORD'><br>"
-    sResponse += EMAIL : "<input type='email' name='EMAIL'><br>"
-    sResponse += "</form>"
-    sResponse += "<FONT SIZE=+1>";
-    //sResponse += "<p>Funktion 1 <a href=\"?pin=FUNCTION1ON\"><button>einschalten</button></a>&nbsp;<a href=\"?pin=FUNCTION1OFF\"><button>ausschalten</button></a></p>";
-    //sResponse += "<p>Funktion 2 <a href=\"?pin=FUNCTION2ON\"><button>einschalten</button></a>&nbsp;<a href=\"?pin=FUNCTION2OFF\"><button>ausschalten</button></a></p>";
-    
-    //////////////////////
-    // react on parameters
-    //////////////////////
-    if (sCmd.length()>0)
-    {
-      // write received command to html page
-      sResponse += "Kommando:" + sCmd + "<BR>";
-      
-      // switch GPIO
-      if(sCmd.indexOf("FUNCTION1ON")>=0)
-      {
-        digitalWrite(2, 1);
-      }
-      else if(sCmd.indexOf("FUNCTION1OFF")>=0)
-      {
-        digitalWrite(2, 0);
-      }
-    }
-    
-    sResponse += "<FONT SIZE=-2>";
-    sResponse += "<BR>Aufrufz&auml;hler="; 
-    sResponse += ulReqcount;
-    sResponse += "<BR>";
-    sResponse += "Stefan Thesen 08/2015<BR>";
-    sResponse += "</body></html>";
-    
-    sHeader  = "HTTP/1.1 200 OK\r\n";
-    sHeader += "Content-Length: ";
-    sHeader += sResponse.length();
-    sHeader += "\r\n";
-    sHeader += "Content-Type: text/html\r\n";
-    sHeader += "Connection: close\r\n";
-    sHeader += "\r\n";
-  }
-  
-  // Send the response to the client
-  client.print(sHeader);
-  client.print(sResponse);
-  
-  // and stop the client
-  client.stop();
-  Serial.println("Client disonnected");
+void loop(void){
+  server.handleClient();
 }
-
